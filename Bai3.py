09bb4404615d34e3d3758cf7edc302f4451f733a
HoangVanSon25 <167545691+HoangVanSon25@users.noreply.github.com>
2026-05-21 07:12:49 +0700

    Add files via upload

--- Bai3.py
+++ Bai3.py
@@ -1,22 +1,22 @@
 choice = 1
 
 while (choice != 3):
-    for i in range(1, 4):
-        print(f"Nhập thông tin nhân viên thứ {i}")
-        employee_id = input("Nhạp mã nhân viên: ").strip()
-        employee_name = input("nhập họ tên nhân viên: ").strip()
-        department = input("Nhập tên phòng ban: ").strip()
-        
-        if employee_id == "" or employee_name == "" or department =="":
-            print("[CẢNH BÁO] Dữ liệu tên hoặc mã không hợp lệ! Hủy bỏ hồ sơ cho nhân viên này.")
-            continue
-        
-        print(f"-----Phiếu Hồ Sơ Điện Tử-----")
-        print (f"Mã Nhân Viên: {employee_id}")
-        print(f"Họ và tên nhân viên: {employee_name}")
-        print(f"Phòng ban đang công tác: {department}")
-    choice = int(input(f"Chọn 3 để thoát!"))
-        
-        
-        
-        
+    print("\n--- NHẬP THÔNG TIN NHÂN VIÊN MỚI ---")
+    employee_id = input("Nhập mã nhân viên: ").strip()
+    employee_name = input("Nhập họ tên nhân viên: ").strip()
+    department = input("Nhập tên phòng ban: ").strip()
+    if employee_id == "":
+        print("[CẢNH BÁO] Mã nhân viên không được để trống! Hủy bỏ hồ sơ.")
+        continue
+    if employee_name == "":
+        print("[CẢNH BÁO] Họ tên nhân viên không được để trống! Hủy bỏ hồ sơ.")
+        continue
+    print("-----Phiếu Hồ Sơ Điện Tử-----")
+    print(f"Mã Nhân Viên: {employee_id}")
+    print(f"Họ và tên nhân viên: {employee_name}")
+    print(f"Phòng ban đang công tác: {department if department != '' else 'Chưa xếp'}")
+    print("-----------------------------")
+    
+    choice = int(input("Nhập số 3 để THOÁT hệ thống, hoặc bấm phím bất kỳ để TIẾP TỤC: "))
+
+print("--- CHƯƠNG TRÌNH KẾT THÚC ---")
